54ae793f3677e467c7f1b9c415e9c76ca41934fc
fatimaulanova <kanalbekovafatima@gmail.com>
2022-08-25 19:04:55 +0600

    initial

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const openPopUp = document.getElementById("open_pop_up");
 const closePopUp = document.getElementById("pop_up_close");
 const popUp = document.getElementById("pop_up")
-const popUpBody = document.getElementById("pop_up_body")
+const popup = document.getElementById("popup")
+const logIn = document.getElementById("btn_log")
+const btnOk = document.getElementById("btn_ok")
 
 openPopUp.addEventListener("click", function (e) {
     e.preventDefault();
@@ -10,4 +12,15 @@ openPopUp.addEventListener("click", function (e) {
 
 closePopUp.addEventListener("click", () =>{
     popUp.classList.remove("active");
+})
+
+logIn.addEventListener("click", ()=>{
+ popUp.remove()
+    popup.style.display= "block"
+    openPopUp.style.display = "none"
+})
+
+btnOk.addEventListener("click", () =>{
+    popup.style.display = "none"
+    openPopUp.style.display = "block"
 })
